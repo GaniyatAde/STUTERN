8e8c388d78c736ca070fce2d2490295a7d60c57a
GaniyatAde <ganiyatas@gmail.com>
2022-07-20 22:36:17 +1100

    This is module_1 Ass_3

---
+++ Module_1_Assignments/Ass_3.py
@@ -0,0 +1,10 @@
+# To find the exponentiation of a number
+print ("This is to find the expenonentiation of a number")
+
+x = int(input ('Enter the number:'))
+y = int(input ('Enter the number:'))
+if y == 0 :
+    
+    print (x," to the power of ", y, "is 1") 
+else:
+    print (x**y, "this is the exponent of", x, "and", y)
---
+++ Module_3_Assignments/ASSIGNMENT_2.py
@@ -0,0 +1,6 @@
+Class cars:
+    
+    #classes hold attributes and methods
+    pass
+cars_Volvo = cars()
+print (type(cars_Volvo))
